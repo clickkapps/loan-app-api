426d84818527c28baf16416932ec63d5f746c415
Daniel Kwakye <dannykay.developer@gmail.com>
2023-05-24 15:04:00 +0000

    :bug: limiting request to GH

--- config/app.php
+++ config/app.php
@@ -168,7 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \Torann\GeoIP\GeoIPServiceProvider::class,
+        Stevebauman\Location\LocationServiceProvider::class,
+
     ])->toArray(),
 
     /*
@@ -184,7 +185,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'GeoIP' => \Torann\GeoIP\Facades\GeoIP::class,
+        'Location' => 'Stevebauman\Location\Facades\Location',
+
     ])->toArray(),
 
 ];
