5d7ea9dac9c08f27a66b26bf59dd13f3d4a16d7c
Daniel Kwakye <dannykay.developer@gmail.com>
2023-05-24 14:45:35 +0000

    :bug: limiting request to GH

---
+++ app/Http/Middleware/LimitRequestsByCountry.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+use Torann\GeoIP\Facades\GeoIP;
+
+class LimitRequestsByCountry
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $ip = request()->ip();
+        $countryCode = GeoIP::getLocation($ip)->country;
+        Log::info("Country code: $countryCode");
+        return $next($request);
+    }
+}
