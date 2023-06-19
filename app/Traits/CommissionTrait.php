ecd4b3e9a86416b241675674d981d11106dfe8c8
Daniel Kwakye <dannykay.developer@gmail.com>
2023-06-19 08:27:05 +0000

    update

--- app/Traits/CommissionTrait.php
+++ app/Traits/CommissionTrait.php
@@ -49,24 +49,34 @@ trait CommissionTrait
             $perc = $commissionConfig->{'percentage_on_repayment_weekends'};
         }
 
+        $commission = $perc; // flat rate for full repayment
+
+        // for partial repayment
+        // if the customer makes part payment, the commission will be a fraction of the laid down commission
+        if($isPartPayment && $loan->{'amount_to_pay'} > 0) {
+            $percentageOfAmountPaid = $amountPaid / $loan->{'amount_to_pay'} * 100;
+            $commission = ($percentageOfAmountPaid / 100) * $commission;
+        }
 //        $commission = $perc / 100 * $amountPaid;
-        $commission = $perc; // flat rate
+
 
         Commission::with([])->create([
             'user_id' => $userId,
             'loan_id' => $loan->{'id'},
             'amount' => $commission,
-            'action' => $isPartPayment ? 'part-payment' : 'full-payment', //'part-payment, full-payment', 'deferment'
+            'action' => $isPartPayment ? 'part-repayment' : 'full-repayment', //'part-payment, full-payment', 'deferment'
             'creator_id' => $loan->{'user_id'}
         ]);
 
-//        add to the agentTasks
-        $existingTask = AgentTask::with([])->where(['user_id' => $userId, 'date' => Carbon::today()])->first();
-        AgentTask::with([])->where(['user_id' => $userId, 'date' => Carbon::today()])
-            ->update([
-            'collected_count' => $existingTask->{'collected_count'} + 1,
-            'collected_amount' => $existingTask->{'collected_amount'} + $amountPaid,
-        ]);
+//        add to the agentTasks if its full repayment
+        if(!$isPartPayment){
+            $existingTask = AgentTask::with([])->where(['user_id' => $userId, 'date' => Carbon::today()])->first();
+            AgentTask::with([])->where(['user_id' => $userId, 'date' => Carbon::today()])
+                ->update([
+                    'collected_count' => $existingTask->{'collected_count'} + 1,
+                    'collected_amount' => $existingTask->{'collected_amount'} + $amountPaid,
+                ]);
+        }
 
         Log::info("Agent commission credited successfully ...");
         $agent->refresh();
